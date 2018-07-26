a79f28af98d7c40ee7917cd51f17b33e7e818192
SheepySean <sold940@aucklanduni.ac.nz>
2018-07-26 19:25:24 +1200

    Introduce some unit tests in preparation for Travis CI

---
+++ src/test/java/com/para11el/scheduler/main/test/GraphResourceIT.java
@@ -0,0 +1,88 @@
+package com.para11el.scheduler.main.test;
+
+import static org.junit.Assert.*;
+import com.para11el.scheduler.graph.GraphConstants;
+import com.para11el.scheduler.graph.GraphFileManager;
+import com.para11el.scheduler.graph.GraphViewManager;
+import com.para11el.scheduler.main.Scheduler;
+import org.graphstream.graph.Graph;
+import org.junit.*;
+
+/**
+ * Simple JUnit test to test the behaviour of the Scheduler.
+ *
+ * @Author Sean Oldfield
+ */
+public class GraphResourceIT {
+	private static GraphFileManager _fileManager;
+	private GraphViewManager _viewManager;
+	
+	@BeforeClass
+	public static void createManagers() {
+	    _fileManager = new GraphFileManager();
+
+	}
+	
+	@Test
+    public void testGraphLoad() {
+	    try {
+            Graph g = _fileManager.readGraphFile(GraphConstants.GRAPH_DIRECTORY.getValue() +
+                    "/" + GraphConstants.SAMPLE_INPUT_FILE.getValue() +
+                    GraphConstants.FILE_EXT.getValue(), "Test");
+            if(!(g instanceof Graph)) {
+                fail();
+            }
+            assertNotEquals(0, g.getNodeCount());
+        } catch (Exception e) {
+	        e.printStackTrace();
+	        fail();
+        }
+    }
+
+    @Test
+    public void testGraphWrite() {
+        try {
+            Graph g = _fileManager.readGraphFile(GraphConstants.GRAPH_DIRECTORY.getValue() +
+                    "/" + GraphConstants.SAMPLE_INPUT_FILE.getValue() +
+                    GraphConstants.FILE_EXT.getValue(), "Test");
+            _fileManager.writeGraphFile(GraphConstants.OUTPUT_PREFIX.getValue()
+                            + GraphConstants.SAMPLE_INPUT_FILE.getValue() + GraphConstants.FILE_EXT.getValue(),
+                    g, true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+
+    @Test
+    public void testViewLabelUnlabel() {
+	    try {
+            Graph g = _fileManager.readGraphFile(GraphConstants.GRAPH_DIRECTORY.getValue() +
+                    "/" + GraphConstants.SAMPLE_INPUT_FILE.getValue() +
+                    GraphConstants.FILE_EXT.getValue(), "Test");
+            _viewManager = new GraphViewManager(g);
+            _viewManager.labelGraph();
+            assertTrue(g.getNode(0).hasAttribute("ui.label"));
+            _viewManager.unlabelGraph();
+            assertFalse(g.getNode(0).hasAttribute("ui.label"));
+        } catch (Exception e) {
+	        e.printStackTrace();
+	        fail();
+        }
+    }
+
+    @Test
+    public void testSchedulerRun() {
+	    try {
+            Scheduler.main(null);
+        } catch (Exception e) {
+	        e.printStackTrace();
+	        fail();
+        }
+    }
+
+    @Test
+    public void testSchedulerRunWithParams() {
+
+    }
+}
