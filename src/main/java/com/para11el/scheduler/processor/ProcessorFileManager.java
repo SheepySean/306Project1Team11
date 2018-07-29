8d6d8cabb94f8777add1fbe39e36a15cef15ff45
Jess-Alcantara <jessica.alcantara97@gmail.com>
2018-07-29 22:36:49 +1200

    Find all paths in graph.
    Peer programming: Jessica Alcantara, Holly Hagenson

--- src/main/java/com/para11el/scheduler/processor/ProcessorFileManager.java
+++ src/main/java/com/para11el/scheduler/processor/ProcessorFileManager.java
@@ -3,7 +3,9 @@ package com.para11el.scheduler.processor;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -14,7 +16,9 @@ import java.util.Map;
 public class ProcessorFileManager {
 	
 	private Map<String,Integer> _nodes = new HashMap<String,Integer>();
+	private Map<String,List<String>> _adjacentNodes = new HashMap<String,List<String>>();
 	private Map<Edge,Integer> _edges = new HashMap<Edge,Integer>();
+	private List<List<String>> _paths = new ArrayList<List<String>>();
 	
     /**
      * Reads in .dot file and extracts nodes and edges (currently only works 
@@ -47,10 +51,73 @@ public class ProcessorFileManager {
             	int cost = Integer.parseInt(graphElements[3].replaceAll("[^0-9]", ""));
             	Edge edge = new Edge(graphElements[0], graphElements[2], cost);
             	_edges.put(edge,cost);
+            	
+            	// Add adjacent node to map
+            	List<String> adj = _adjacentNodes.containsKey(graphElements[0]) ? 
+            			_adjacentNodes.get(graphElements[0]) : new ArrayList<String>();
+            	adj.add(graphElements[2]);
+            	_adjacentNodes.put(graphElements[0], adj);
             }
  
         }
         reader.close();
+	}
+	
+    /**
+     * Returns the list of root nodes of a graph.
+     * @return List of root nodes
+     * @author Jessica Alcantara, Holly Hagenson
+     */
+	public List<String> getRoots() {
+		List<String> roots = new ArrayList<String>(_nodes.keySet());
+		for (String s : _nodes.keySet()) {
+			for (Edge e : _edges.keySet()) {
+				if (e.getTarget().equals(s)) {
+					roots.remove(s);
+				}
+			}
+		}
+
+		return roots;
+	}
+	
+    /**
+     * Calculates all possible path of multiple entry and exits of a graph.
+     * @return List of paths
+     * @author Jessica Alcantara, Holly Hagenson
+     */
+	public List<List<String>> calculateStateSpace() {
+		List<String> roots = getRoots();
+		
+		// Find paths for each entry of a graph
+		for (String entry : roots) {
+			List<String> path = new ArrayList<String>();
+			path.add(entry);
+			findPath(path, entry);
+		}
+
+		return _paths;
+	}
+	
+    /**
+     * Finds all paths from an entry node.
+     * @author Jessica Alcantara, Holly Hagenson
+     */
+	public void findPath(List<String> path, String node) {
+		if (_adjacentNodes.keySet().contains(node)) {
+			List<String> adj = _adjacentNodes.get(node);
+			
+			// Find all possible paths through each adjacent nodes
+			for (String n : adj) {
+				List<String> newPath = new ArrayList<String>(path);
+				newPath.add(n);
+				findPath(newPath,n);
+			}
+			
+		// Add path to list at leaf node 
+		} else {
+			_paths.add(path);
+		}
 	}
 
 }
