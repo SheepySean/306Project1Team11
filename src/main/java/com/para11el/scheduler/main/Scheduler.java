a7bf1309ced28247388853ae7750658494b59a77
Tina Chen <tche278@aucklanduni.ac.nz>
2018-08-02 10:27:15 +1200

    Catch ArrayOutofBoundsException so output file can be automatically renamed

--- src/main/java/com/para11el/scheduler/main/Scheduler.java
+++ src/main/java/com/para11el/scheduler/main/Scheduler.java
@@ -11,96 +11,102 @@ import java.io.IOException;
  */
 public class Scheduler {
 
-    private static Graph _inGraph = null;
+	private static Graph _inGraph = null;
 
-    private static String _filename = null;
-    private static int _scheduleProcessors = 0;
-    private static int _numCores = 0;
-    private static String _outputFilename = null;
-    private static boolean _visualise = false;
+	private static String _filename = null;
+	private static int _scheduleProcessors = 0;
+	private static int _numCores = 0;
+	private static String _outputFilename = null;
+	private static boolean _visualise = false;
 
-    /**
-     * Entry point for the program
-     * @param args Command line arguments
-     */
-    public static void main(String[] args) {
-        // Read the parameters provided on the command line
-        try {
-            Scheduler.readParameters(args);
-        } catch (ParameterLengthException e) {
-            System.out.println("At least 2 parameters required");
-            return; // Exit
-        } catch (NumberFormatException e) {
-            System.out.println("Please ensure that processors and cores specified " +
-                    "are numbers");
-            return;
-        }
+	/**
+	 * Entry point for the program
+	 * @param args Command line arguments
+	 */
+	public static void main(String[] args) {
+		// Read the parameters provided on the command line
+		try {
+			Scheduler.readParameters(args);
+		} catch (ParameterLengthException e) {
+			System.out.println("At least 2 parameters required");
+			return; // Exit
+		} catch (NumberFormatException e) {
+			System.out.println("Please ensure that processors and cores specified " +
+					"are numbers");
+			return;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Please ensure that number of cores is specified");
+			return;
+		}
 
-        // Read the supplied graph file in
-        GraphFileManager fileManager = new GraphFileManager();
-        try {
-            _inGraph = fileManager.readGraphFile(_filename,
-                    "Example Graph");
-        } catch(IOException e) {
-            System.out.println("Cannot find the specified input file '" + _filename +
-            "'");
-            return;
-        }
+		// Read the supplied graph file in
+		GraphFileManager fileManager = new GraphFileManager();
+		try {
+			_inGraph = fileManager.readGraphFile(_filename,
+					"Example Graph");
+		} catch(IOException e) {
+			System.out.println("Cannot find the specified input file '" + _filename +
+					"'");
+			return;
+		}
 
-        // For viewing the Graph
-        GraphViewManager viewManager = new GraphViewManager(_inGraph);
-        viewManager.labelGraph();
+		// For viewing the Graph
+		GraphViewManager viewManager = new GraphViewManager(_inGraph);
+		viewManager.labelGraph();
 
-        if(_visualise) {
-            _inGraph.display();
-        }
-        // Name the file if no specific output name was provided
-        if(_outputFilename == null) {
-            _outputFilename = _filename.substring(0, _filename.lastIndexOf('.'))
-                    + "-output" + GraphConstants.FILE_EXT.getValue();
-        }
-        // Write the output file
-        try {
-            fileManager.writeGraphFile(_outputFilename,
-                    _inGraph, true);
-        } catch(IOException e) {
-            System.out.println("Unable to write the graph to the file '" + _filename +
-                    "'");
-        }
-    }
+		if(_visualise) {
+			_inGraph.display();
+		}
+		// Name the file if no specific output name was provided
+		if(_outputFilename == null) {
+			_outputFilename = _filename.substring(0, _filename.lastIndexOf('.'))
+					+ "-output" + GraphConstants.FILE_EXT.getValue();
+		}
+		// Write the output file
+		try {
+			fileManager.writeGraphFile(_outputFilename,
+					_inGraph, true);
+		} catch(IOException e) {
+			System.out.println("Unable to write the graph to the file '" + _filename +
+					"'");
+		}
+	}
 
-    /**
-     * Read supplied command line arguments into the scheduler fields
-     * @param params Command line arguments
-     * @throws ParameterLengthException Thrown if less than the required number of parameters is provided
-     * @throws NumberFormatException Thrown if expected number parameters are not numbers
-     * @author Sean Oldfield
-     */
-    private static void readParameters(String[] params)
-            throws ParameterLengthException, NumberFormatException {
-        // If the required parameters weren't specified
-        if(params.length < ParameterType.REQUIRED_PARAMS) {
-            throw new ParameterLengthException();
-        }
+	/**
+	 * Read supplied command line arguments into the scheduler fields
+	 * @param params Command line arguments
+	 * @throws ParameterLengthException Thrown if less than the required number of parameters is provided
+	 * @throws NumberFormatException Thrown if expected number parameters are not numbers
+	 * @author Sean Oldfield
+	 */
+	private static void readParameters(String[] params)
+			throws ParameterLengthException, NumberFormatException, ArrayIndexOutOfBoundsException {
+		// If the required parameters weren't specified
+		if(params.length < ParameterType.REQUIRED_PARAMS) {
+			throw new ParameterLengthException();
+		}
 
-        // Read the required parameters
-        _filename = params[0];
-        _scheduleProcessors = Integer.parseInt(params[1]);
+		// Read the required parameters
+		_filename = params[0];
+		_scheduleProcessors = Integer.parseInt(params[1]);
 
-        // Read the additional parameters if there are any
-        for(int i = ParameterType.REQUIRED_PARAMS; i < params.length; i++) {
-            switch (params[i]) {
-                case ParameterType.PARRALISE:
-                    _numCores = Integer.parseInt(params[i + 1]);
-                    break;
-                case ParameterType.VISUALISE:
-                    _visualise = true;
-                    break;
-                case ParameterType.OUTPUT_FILE:
-                    _outputFilename = params[i + 1];
-                    break;
-            }
-        }
-    }
-
+		// Read the additional parameters if there are any
+		for(int i = ParameterType.REQUIRED_PARAMS; i < params.length; i++) {
+			switch (params[i]) {
+			case ParameterType.PARRALISE:
+				_numCores = Integer.parseInt(params[i + 1]);
+				break;
+			case ParameterType.VISUALISE:
+				_visualise = true;
+				break;
+			case ParameterType.OUTPUT_FILE:
+				try {
+					_outputFilename = params[i + 1];
+					break;
+				} catch (ArrayIndexOutOfBoundsException e) {
+					
+				}
+			}
+		}
+	}
 }
