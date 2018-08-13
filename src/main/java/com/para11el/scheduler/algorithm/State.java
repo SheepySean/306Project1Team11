06fc4cc4aed1309e779880fe645d60d71811f7ed
Jess-Alcantara <jessica.alcantara97@gmail.com>
2018-08-13 17:43:25 +1200

    Refactor state model

--- src/main/java/com/para11el/scheduler/algorithm/State.java
+++ src/main/java/com/para11el/scheduler/algorithm/State.java
@@ -14,7 +14,6 @@ public class State {
 	private Node _node;
 	private Node _parent;
 	private int _cost;
-	private int _scheduleLength;
 	
 	public State() {}
 	
@@ -23,15 +22,13 @@ public class State {
 	 * 
 	 * @param node a node in input graph
 	 * @param cost cost calculated by cost function
-	 * @param scheduleLength the length of the partial schedule solution
 	 * 
 	 * @author Jessica Alcantara
 	 */
-	public State(Node node, Node parent, int cost, int scheduleLength) {
+	public State(Node node, Node parent, int cost) {
 		_node = node;
 		_parent = parent;
 		_cost = cost;
-		_scheduleLength = scheduleLength;
 	}
 	
 	public Node getNode() {
@@ -44,10 +41,6 @@ public class State {
 	
 	public int getCost() {
 		return _cost;
-	}
-	
-	public int getScheduleLength() {
-		return _scheduleLength;
 	}
 	
 	public void setCost(int cost) {
