4506f41454b5d3eb15b1623883ce058c16701ef4
holly.hagenson <holly.hagenson@gmail.com>
2018-08-13 23:36:56 +1200

    Add test cases to test A* algorithm.

--- src/test/java/com/para11el/scheduler/algorithm/test/AStarAlgorithmIT.java
+++ src/test/java/com/para11el/scheduler/algorithm/test/AStarAlgorithmIT.java
@@ -7,7 +7,9 @@ import java.util.Collection;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
+import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.SingleGraph;
 import org.junit.Test;
 
 import com.para11el.scheduler.algorithm.AStarAlgorithm;
@@ -21,6 +23,7 @@ import com.para11el.scheduler.algorithm.Task;
  *
  */
 public class AStarAlgorithmIT {
+	private static Graph _graph1;
 	
 	/**
 	 * Unit test for the ascending ordering of the state comparator.
@@ -65,10 +68,93 @@ public class AStarAlgorithmIT {
 		assertEquals(totalWeight, 17); 	
 	}
 	
+	/**
+	 * Unit test for getting the earliest start time of a node. 
+	 * @author Holly Hagenson
+	 */
 	@Test
-	public void testGetEarliestStartTime() {
-		AStarAlgorithm am = new AStarAlgorithm(); 
+	public void testGetEarliestStartTime() {		
+		createGraph(); 
 		
+		ArrayList<Task> testSchedule = new ArrayList<Task>(); 
+		testSchedule.add(new Task(_graph1.getNode("1"),0,1));
+		testSchedule.add(new Task(_graph1.getNode("2"),3,1));
+		testSchedule.add(new Task(_graph1.getNode("3"),5,2));
+		
+		AStarAlgorithm am = new AStarAlgorithm(); 
+		int start = am.getEarliestStartTime(_graph1.getNode("4"), testSchedule, 1);
+		
+		assertEquals(start, 7); 
 	}
+	
+	@Test
+	public void testScheduleContainsNode() {
+		ArrayList<Task> testSchedule = new ArrayList<Task>();
+		Node node = new MockNode(null,"C",3);  
+		testSchedule.add(new Task(new MockNode(null,"A",2),0,1));
+		testSchedule.add(new Task(new MockNode(null,"B",2),1,2));
+		testSchedule.add(new Task(node,0,3));
+		testSchedule.add(new Task(new MockNode(null,"D",1),3,1));
+		testSchedule.add(new Task(new MockNode(null,"E",1),4,2));
+		testSchedule.add(new Task(new MockNode(null,"F",2),3,3));
+		
+		AStarAlgorithm am = new AStarAlgorithm();
+		assertTrue(am.scheduleContainsNode(node, testSchedule));
+	}
+	
+	@Test 
+	public void testBuildSolution(){
+		createGraph(); 
+		
+		AStarAlgorithm as = new AStarAlgorithm(_graph1, 2); 
+		
+		ArrayList<Task> solution = as.buildSolution(); 
+		
+		for (Task t : solution){
+			if (t.getNode().getId().equals("1")){
+				assertEquals(t.getStartTime(), 0);
+				assertEquals(t.getProcessor(), 1);
+			}
+			if (t.getNode().getId().equals("2")){
+				assertEquals(t.getStartTime(), 4);
+				assertEquals(t.getProcessor(), 1);
+			}
+			if (t.getNode().getId().equals("3")){
+				assertEquals(t.getStartTime(), 7);
+				assertEquals(t.getProcessor(), 2);
+			}
+			if (t.getNode().getId().equals("4")){
+				assertEquals(t.getStartTime(), 10);
+				assertEquals(t.getProcessor(), 2);
+			}
+			if (t.getNode().getId().equals("5")){
+				assertEquals(t.getStartTime(), 7);
+				assertEquals(t.getProcessor(), 1);
+			}
+		}
+	}
+	
+	public void createGraph(){
+		_graph1 = new SingleGraph("graphWithMultipleProcessors");
+		_graph1.addNode("1");
+		_graph1.addNode("2");
+		_graph1.addNode("3");
+		_graph1.addNode("4");
+		_graph1.addNode("5");
+		_graph1.getNode("1").addAttribute("Weight", 3.0);
+		_graph1.getNode("2").addAttribute("Weight", 4.0);
+		_graph1.getNode("3").addAttribute("Weight", 2.0);
+		_graph1.getNode("4").addAttribute("Weight", 1.0);
+		_graph1.getNode("5").addAttribute("Weight", 5.0);
+		_graph1.addEdge("1 -> 2", "1", "2", true);
+		_graph1.addEdge("1 -> 3", "1", "3", true);
+		_graph1.addEdge("2 -> 4", "2", "4", true);
+		_graph1.addEdge("2 -> 5", "2", "5", true);
+		_graph1.getEdge("1 -> 2").setAttribute("Weight", 1.0);
+		_graph1.getEdge("1 -> 3").setAttribute("Weight", 2.0);
+		_graph1.getEdge("2 -> 4").setAttribute("Weight", 3.0);
+		_graph1.getEdge("2 -> 5").setAttribute("Weight", 5.0);
+	}
+	
 
 }
