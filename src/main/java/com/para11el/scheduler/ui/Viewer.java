8bf5e78ffc150378e926a065b6d0bfa42839bcc4
SheepySean <sold940@aucklanduni.ac.nz>
2018-08-16 11:31:45 +1200

    Update Viewer to pass parameters to the GUI Controller

--- src/main/java/com/para11el/scheduler/ui/Viewer.java
+++ src/main/java/com/para11el/scheduler/ui/Viewer.java
@@ -2,11 +2,17 @@ package com.para11el.scheduler.ui;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -18,8 +24,12 @@ public class Viewer extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+
+        final Popup popup = new Popup(); popup.setX(300); popup.setY(200);
+        popup.getContent().addAll(new Circle(25, 25, 50, Color.AQUAMARINE));
         Stage stage = null;
         try{
+            ViewerPaneController.setParameters(getParameters().getRaw());
             Parent root = FXMLLoader.load(getClass().getResource("/fxml/ViewerPane.fxml")); // Load the fxml pane
             Scene scene = new Scene(root);
 
@@ -30,6 +40,7 @@ public class Viewer extends Application {
             stage.setScene(scene);
             //stage.setResizable(false);
             stage.setTitle("Para11el - Task Scheduler");
+
             stage.show();
         } catch(Exception e) {
             e.printStackTrace();
