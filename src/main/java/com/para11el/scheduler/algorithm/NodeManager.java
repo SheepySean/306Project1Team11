8bc7ad3429dbaad62e6efb91a8a272909d2a2327
Jess-Alcantara <jessica.alcantara97@gmail.com>
2018-08-16 15:32:04 +1200

    Add node manager

--- src/main/java/com/para11el/scheduler/algorithm/NodeManager.java
+++ src/main/java/com/para11el/scheduler/algorithm/NodeManager.java
@@ -4,42 +4,50 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map.Entry;
 
 import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 
 public class NodeManager {
-	
+
 	private Graph _graph;
-	private HashMap<Node,Integer> _criticalPath = new HashMap<Node,Integer>();
-	
+	private int _maxCost;
+	private HashMap<Node,Integer> _bottomLevel = new HashMap<Node,Integer>();
+
 	public NodeManager(Graph graph) {
 		_graph = graph;
 		initialise();
 	}
-	
+
 	public void initialise() {
 		_graph.nodes().forEach((node)->{
-			calculateBottomLevel(node);
+			_maxCost = 0;
+			calculateBottomLevel(node, 0);
+			_bottomLevel.put(node, _maxCost);
 		});
 	}
-	
-	public void calculateBottomLevel(Node node) {
+
+	public void calculateBottomLevel(Node node, int cost) {
+		int nodeCost = cost + ((Number)node.getAttribute("Weight")).intValue();
 		if (node.getOutDegree() == 0) {
-			_criticalPath.put(node, ((Number)node.getAttribute("Weight")).intValue());
+			if (nodeCost > _maxCost) {
+				_maxCost = nodeCost;
+			}
 		} else {
-			/*Iterator<Edge> edges = source.leavingEdges().iterator();
+			Iterator<Edge> edges = node.leavingEdges().iterator();
 			while(edges.hasNext()){
-				Edge currentEdge = edges.next(); 
-				if (currentEdge.getNode0().equals(source)){
-					List<Node> newPath = new ArrayList<Node>(path); 
-					newPath.add(currentEdge.getNode1());
-					findLongestPath(newPath, currentEdge.getNode1());					
-				}
-			}*/
+				Edge currentEdge = edges.next();
+				calculateBottomLevel(currentEdge.getTargetNode(), nodeCost);
+			}
 		}
-		
+	}
+	
+	public void printCriticalPaths() {
+		for (Entry<Node, Integer> e : _bottomLevel.entrySet()) {
+			System.out.println(e.getKey().getId() + " " + e.getValue());
+		}
 	}
 
 }
