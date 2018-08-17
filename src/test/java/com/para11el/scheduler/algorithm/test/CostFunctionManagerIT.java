fd4c1567d7ac92570b6db6dc1d8eccb6d553594b
holly.hagenson <holly.hagenson@gmail.com>
2018-08-17 12:28:52 +1200

    Update cost function testing class

--- src/test/java/com/para11el/scheduler/algorithm/test/CostFunctionManagerIT.java
+++ src/test/java/com/para11el/scheduler/algorithm/test/CostFunctionManagerIT.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.para11el.scheduler.algorithm.AStarAlgorithm;
 import com.para11el.scheduler.algorithm.CostFunctionManager;
 import com.para11el.scheduler.algorithm.NodeManager;
 import com.para11el.scheduler.algorithm.State;
@@ -23,12 +24,19 @@ import org.graphstream.graph.implementations.SingleGraph;
  *
  */
 public class CostFunctionManagerIT {
-	private static Graph _graph1;
+	private static Graph _testGraph;
+	private static CostFunctionManager _cfm; 
+	private static TestGraphManager _tgManager; 
 	private static NodeManager _nm; 
+	private static AStarAlgorithm _aStar; 
+	private ArrayList<Task> _tasks;
+	private int _processors; 
 	
 	@BeforeClass
 	public static void initialise(){
-		_nm = new NodeManager(_graph1); 
+		_tgManager = new TestGraphManager(); 
+		_testGraph = _tgManager.createGraph();
+		_nm = new NodeManager(_testGraph);  
 	}
 
 	/**
@@ -72,90 +80,54 @@ public class CostFunctionManagerIT {
 	}
 	
 	/**
+	 * Test that the task of a given node is returned by findNodeTask.
+	 * 
+	 * @author Holly Hagenson
+	 */
+	@Test
+	public void testFindNodeTask(){
+		_aStar = new AStarAlgorithm(_testGraph, 1);
+		
+		_tasks = _aStar.buildSolution();
+		
+		_cfm = new CostFunctionManager(_nm, 15, 1); 
+		
+		assertEquals(_cfm.findNodeTask(_testGraph.getNode("2"), _tasks), _tasks.get(1)); 
+	}
+	
+	/**
 	 * Unit test to calculate the critical path estimate of a graph.
+	 * 
 	 * @author Holly Hagenson
 	 */
 	@Test
 	public void testCalculateCriticalPathEstimate(){
-		createGraph();
+		_aStar = new AStarAlgorithm(_testGraph, 1);
+		_tasks = _aStar.buildSolution();
 		
-		Node nlast = _graph1.getNode("2");
-		nlast.setAttribute("Start", "3");
-		Task lastTask = new Task(nlast, 3, 1);
+		_cfm = new CostFunctionManager(_nm, 15, 2); 
 		
-		CostFunctionManager cfm = new CostFunctionManager(_nm, 15, 2);
+		Node nlast = _testGraph.getNode("4"); 
+
+		int criticalPathEstimate = _cfm.criticalPathEstimate(_tasks, nlast);
 		
-		ArrayList<Task> testSolution = new ArrayList<Task>();
-		testSolution.add(new Task(_graph1.getNode("1"),0,1));
-		testSolution.add(new Task(_graph1.getNode("2"),3,1));
-		
-		//int criticalPathEstimate = cfm.calculateCriticalPathEstimate(lastTask, testSolution);
-		
-		//assertEquals(criticalPathEstimate, 12);
+		assertEquals(criticalPathEstimate, 13);
 	}
 	
-	/**
-	 * Unit test to calculate bottom level path of given node.
-	 * @author Holly Hagenson
-	 */
-	@Test
-	public void testCalculateBottomLevel(){
-		createGraph(); 
-		int processors = 2; 
-		CostFunctionManager cfm = new CostFunctionManager(_nm, 15, processors);
-		
-		//int bottomLevel = cfm.bottomLevel(_graph1.getNode("2"));
-		//assertEquals(9, bottomLevel); 
-	}
 	
 	/**
-	 * Unit test to calculate cost function of a state.
+	 * Unit test to calculate the overall cost function of a state.
 	 * @author Holly Hagenson 
 	 */
-	/*@Test
+	@Test
 	public void testCostFunction(){
-		createGraph(); 
+		_aStar = new AStarAlgorithm(_testGraph, 1);
+		_tasks = _aStar.buildSolution();
 		
-		State parentState = new State();
-		parentState.setCost(12);
+		_cfm = new CostFunctionManager(_nm, 15, 2);
 		
-		ArrayList<Task> testSolution = new ArrayList<Task>();
-		testSolution.add(new Task(_graph1.getNode("1"),0,1));
-		testSolution.add(new Task(_graph1.getNode("2"),3,1));
-		
-		CostFunctionManager cfm = new CostFunctionManager(15, 2);
-		
-		int cost = cfm.calculateCostFunction(parentState, _graph1.getNode("2"), testSolution); 
-		
-		assertEquals(cost, 12); 
-	}*/
-	
-	/**
-	 * Create graphstream graph to use for testing.
-	 * @author Holly Hagenson
-	 */
-	public static void createGraph(){
-		_graph1 = new SingleGraph("graphWithMultipleProcessors");
-		_graph1.addNode("1");
-		_graph1.addNode("2");
-		_graph1.addNode("3");
-		_graph1.addNode("4");
-		_graph1.addNode("5");
-		_graph1.getNode("1").setAttribute("Weight", 3.0);
-		_graph1.getNode("2").setAttribute("Weight", 4.0);
-		_graph1.getNode("3").setAttribute("Weight", 2.0);
-		_graph1.getNode("4").setAttribute("Weight", 1.0);
-		_graph1.getNode("5").setAttribute("Weight", 5.0);
-		_graph1.addEdge("1 -> 2", "1", "2", true);
-		_graph1.addEdge("1 -> 3", "1", "3", true);
-		_graph1.addEdge("2 -> 4", "2", "4", true);
-		_graph1.addEdge("2 -> 5", "2", "5", true);
-		_graph1.getEdge("1 -> 2").setAttribute("Weight", 1.0);
-		_graph1.getEdge("1 -> 3").setAttribute("Weight", 2.0);
-		_graph1.getEdge("2 -> 4").setAttribute("Weight", 3.0);
-		_graph1.getEdge("2 -> 5").setAttribute("Weight", 5.0);
-		
+		int cost = _cfm.calculateCostFunction(null, _testGraph.getNode("4"), _tasks); 
+		assertEquals(cost, 13); 
 	}
-	
-	
+		
 }
