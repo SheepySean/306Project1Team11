9b42694f949254925a6ca9f684968ffc112709ec
Tina Chen <tche278@aucklanduni.ac.nz>
2018-08-17 13:59:47 +1200

    Add processor labels and adjust cell calculation

--- src/main/java/com/para11el/scheduler/ui/ViewerPaneController.java
+++ src/main/java/com/para11el/scheduler/ui/ViewerPaneController.java
@@ -36,374 +36,393 @@ import java.util.Random;
 
 
 public class ViewerPaneController {
-    private static FxViewer _viewer;
-    private static Camera _camera;
-    private FxDefaultView viewPanel;
+	private static FxViewer _viewer;
+	private static Camera _camera;
+	private FxDefaultView viewPanel;
 
-    private static AnimationTimer _timer;
+	private static AnimationTimer _timer;
 
-    private static String _inputFile;
-    private static String _outputFile;
-    private static String _processors;
-    private static String _cores;
-    private static long _startTime;
+	private static String _inputFile;
+	private static String _outputFile;
+	private static String _processors;
+	private static String _cores;
+	private static long _startTime;
 
-    private int _cellWidth;
-    private int _cellHeight = 20;
+	private int _cellWidth;
+	private int _cellHeight = 20;
 
-    @FXML
-    private AnchorPane graphContainer;
-
-
-    @FXML
-    private Label timerLabel;
-
-    @FXML
-    private Text inputFileText;
-
-    @FXML
-    private Text processorsText;
-
-    @FXML
-    private Text coresText;
-
-    @FXML
-    private Text outputFileText;
-
-    @FXML
-    private TilePane colLabelTile;
-
-    @FXML
-    private TilePane tile;
-
-    @FXML
-    private ScrollPane scrollPane;
+	@FXML
+	private AnchorPane graphContainer;
 
 
-    /**
-     * Initialize some of the GUI's components to initial states
-     */
-    @FXML
-    public void initialize() {
+	@FXML
+	private Label timerLabel;
 
-    	scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+	@FXML
+	private Text inputFileText;
 
-    	setCellSize(Integer.parseInt(_processors));
+	@FXML
+	private Text processorsText;
 
-    	initialisePane(30);
-    	initialiseLabel(30);
+	@FXML
+	private Text coresText;
 
+	@FXML
+	private Text outputFileText;
 
-		// set cell: processor, start time, length of time, colour (string)
-		setCell("a", 2, 3, 2, generateColours());
-		setCell("b", 4, 0, 7, generateColours());
+	@FXML
+	private TilePane colLabelTile;
 
-		setCell("b2", 4, 7, 7, generateColours());
+	@FXML
+	private TilePane tile;
 
-		setCell("c", 3, 18, 12, generateColours());
-
-		setCell("b", 1, 0, 1, generateColours());
-
-		setCell("b", 1, 1, 1, generateColours());
-
-		setCell("b", 1, 7, 4, generateColours());
+	@FXML
+	private ScrollPane scrollPane;
 
 
-		// testing more processors
+	/**
+	 * Initialize some of the GUI's components to initial states
+	 */
+	 @FXML
+	 public void initialize() {
+
+		 scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+
+		 setCellSize(Integer.parseInt(_processors));
+
+		 initialisePane(30);
+		 initialiseLabel(30);
 
 
-        // Embed GraphStream graph into the GUI
-        _viewer.addDefaultView(false, _viewer.newDefaultGraphRenderer());
-        _viewer.enableAutoLayout();
-        viewPanel = (FxDefaultView) _viewer.getDefaultView();
-        viewPanel.setFocusTraversable(true); // Allow the keyboard shortcuts
-        viewPanel.setMaxHeight(357); // So it fits
-        viewPanel.setMaxWidth(598);
-        viewPanel.requireFocus();
-        _camera = _viewer.getDefaultView().getCamera();
-        graphContainer.getChildren().add(viewPanel); // Add it to its container
+		 // set cell: processor, start time, length of time, colour (string)
+		 setCell("a", 2, 3, 2, generateColours());
+		 setCell("b", 4, 0, 7, generateColours());
 
-        inputFileText.setText(_inputFile);
-        coresText.setText(_cores);
-        processorsText.setText(_processors);
-        outputFileText.setText(_outputFile);
+		 setCell("b2", 4, 7, 7, generateColours());
+
+		 setCell("c", 3, 18, 12, generateColours());
+
+		 setCell("b", 1, 0, 1, generateColours());
+
+		 setCell("b", 1, 1, 1, generateColours());
+
+		 setCell("b", 1, 7, 4, generateColours());
 
 
-        // Set the timer for elapsing the program run time
-        _timer = new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                long elapsedMillis = System.currentTimeMillis() - _startTime ;
-                String timeLabel = String.format("%02d:%02d:%02d",
-                        (elapsedMillis / 60000),
-                        ((elapsedMillis % 60000) / 1000),
-                        ((elapsedMillis % 1000) / 10));
-                timerLabel.setText(timeLabel);
-            }
-        };
+		 // testing more processors
 
-        toggleTimer(true); // Start the timer
-    }
 
-    public static void setViewer(FxViewer viewer) {
-        _viewer = viewer;
-    }
+		 // Embed GraphStream graph into the GUI
+		 _viewer.addDefaultView(false, _viewer.newDefaultGraphRenderer());
+		 _viewer.enableAutoLayout();
+		 viewPanel = (FxDefaultView) _viewer.getDefaultView();
+		 viewPanel.setFocusTraversable(true); // Allow the keyboard shortcuts
+		 viewPanel.setMaxHeight(357); // So it fits
+		 viewPanel.setMaxWidth(598);
+		 viewPanel.requireFocus();
+		 _camera = _viewer.getDefaultView().getCamera();
+		 graphContainer.getChildren().add(viewPanel); // Add it to its container
 
-    /**
-     * Zoom out on the graph view
-     * @param event
-     * @Author Sean Oldfield
-     */
-    @FXML
-    private void zoomInAction(ActionEvent event) {
-        Platform.runLater(() -> {
-            _camera.setViewPercent(Math.max(0.0001f,
-                    _camera.getViewPercent() * 0.7f));
-        });
-    }
+		 inputFileText.setText(_inputFile);
+		 coresText.setText(_cores);
+		 processorsText.setText(_processors);
+		 outputFileText.setText(_outputFile);
 
-    /**
-     * Zoom out on the graph view
-     * @param event
-     * @Author Sean Oldfield
-     */
-    @FXML
-    private void zoomOutAction(ActionEvent event) {
-        Platform.runLater(() -> {
-            _camera.setViewPercent(_camera.getViewPercent() * 1.3f);
-        });
-    }
 
-    /**
-     * Recenter the camera to default on the graph view
-     * @param event
-     * @Author Sean Oldfield
-     */
-    @FXML
-    private void resetViewAction(ActionEvent event) {
-        Platform.runLater(() -> {
-              _camera.resetView();
-        });
-    }
+		 // Set the timer for elapsing the program run time
+		 _timer = new AnimationTimer() {
+			 @Override
+			 public void handle(long now) {
+				 long elapsedMillis = System.currentTimeMillis() - _startTime ;
+				 String timeLabel = String.format("%02d:%02d:%02d",
+						 (elapsedMillis / 60000),
+						 ((elapsedMillis % 60000) / 1000),
+						 ((elapsedMillis % 1000) / 10));
+				 timerLabel.setText(timeLabel);
+			 }
+		 };
 
-    /**
-     * Pan the camera on the graph view up
-     * @param event
-     * @Author Sean Oldfield
-     */
-    @FXML
-    private void panUpAction(ActionEvent event) {
-        Platform.runLater(() -> {
-            double delta = calculateDelta();
+		 toggleTimer(true); // Start the timer
+	 }
 
-            Point3 p = _camera.getViewCenter();
-            _camera.setViewCenter(p.x, p.y + delta, 0);
-        });
-    }
+	 public static void setViewer(FxViewer viewer) {
+		 _viewer = viewer;
+	 }
 
-    /**
-     * Pan the camera on the graph view down
-     * @param event
-     * @Author Sean Oldfield
-     */
-    @FXML
-    private void panDownAction(ActionEvent event) {
-        Platform.runLater(() -> {
-            double delta = calculateDelta();
+	 /**
+	  * Zoom out on the graph view
+	  * @param event
+	  * @Author Sean Oldfield
+	  */
+	 @FXML
+	 private void zoomInAction(ActionEvent event) {
+		 Platform.runLater(() -> {
+			 _camera.setViewPercent(Math.max(0.0001f,
+					 _camera.getViewPercent() * 0.7f));
+		 });
+	 }
 
-            Point3 p = _camera.getViewCenter();
-            _camera.setViewCenter(p.x, p.y - delta, 0);
-        });
-    }
+	 /**
+	  * Zoom out on the graph view
+	  * @param event
+	  * @Author Sean Oldfield
+	  */
+	 @FXML
+	 private void zoomOutAction(ActionEvent event) {
+		 Platform.runLater(() -> {
+			 _camera.setViewPercent(_camera.getViewPercent() * 1.3f);
+		 });
+	 }
 
-    /**
-     * Pan the camera on the graph view right
-     * @param event
-     * @Author Sean Oldfield
-     */
-    @FXML
-    private void panRightAction(ActionEvent event) {
-        Platform.runLater(() -> {
-            double delta = calculateDelta();
+	 /**
+	  * Recenter the camera to default on the graph view
+	  * @param event
+	  * @Author Sean Oldfield
+	  */
+	 @FXML
+	 private void resetViewAction(ActionEvent event) {
+		 Platform.runLater(() -> {
+			 _camera.resetView();
+		 });
+	 }
 
-            Point3 p = _camera.getViewCenter();
-            _camera.setViewCenter(p.x + delta, p.y, 0);
-        });
-    }
+	 /**
+	  * Pan the camera on the graph view up
+	  * @param event
+	  * @Author Sean Oldfield
+	  */
+	 @FXML
+	 private void panUpAction(ActionEvent event) {
+		 Platform.runLater(() -> {
+			 double delta = calculateDelta();
 
-    /**
-     * Pan the camera on the graph view left
-     * @param event
-     * @Author Sean Oldfield
-     */
-    @FXML
-    private void panLeftAction(ActionEvent event) {
-        Platform.runLater(() -> {
-            double delta = calculateDelta();
+			 Point3 p = _camera.getViewCenter();
+			 _camera.setViewCenter(p.x, p.y + delta, 0);
+		 });
+	 }
 
-            Point3 p = _camera.getViewCenter();
-            _camera.setViewCenter(p.x - delta, p.y, 0);
-        });
-    }
+	 /**
+	  * Pan the camera on the graph view down
+	  * @param event
+	  * @Author Sean Oldfield
+	  */
+	 @FXML
+	 private void panDownAction(ActionEvent event) {
+		 Platform.runLater(() -> {
+			 double delta = calculateDelta();
 
-    /**
-     * Set the graph to be have a hierarchical layout
-     * @param event
-     * @Author Sean Oldfield
-     */
-    @FXML
-    void setHierarchicalLayout(ActionEvent event) {
-        _viewer.disableAutoLayout();
-        _viewer.enableAutoLayout(new HierarchicalLayout());
-    }
+			 Point3 p = _camera.getViewCenter();
+			 _camera.setViewCenter(p.x, p.y - delta, 0);
+		 });
+	 }
 
-    /**
-     * Set the graph to be have a linear logarithm layout
-     * @param event
-     * @Author Sean Oldfield
-     */
-    @FXML
-    void setLinLogLayout(ActionEvent event) {
-        _viewer.disableAutoLayout();
-        _viewer.enableAutoLayout(new LinLog());
-    }
+	 /**
+	  * Pan the camera on the graph view right
+	  * @param event
+	  * @Author Sean Oldfield
+	  */
+	 @FXML
+	 private void panRightAction(ActionEvent event) {
+		 Platform.runLater(() -> {
+			 double delta = calculateDelta();
 
-    /**
-     * Set the graph to be have a spring box layout
-     * @param event
-     * @Author Sean Oldfield
-     */
-    @FXML
-    void setSpringBoxLayout(ActionEvent event) {
-        _viewer.disableAutoLayout();
-        _viewer.enableAutoLayout(new SpringBox());
-    }
+			 Point3 p = _camera.getViewCenter();
+			 _camera.setViewCenter(p.x + delta, p.y, 0);
+		 });
+	 }
 
-    /**
-     * Give graph view focus when clicked on i.e. allow it to be accessed by keyboard shortcuts
-     * @param event
-     * @Author Sean Oldfield
-     */
-    @FXML
-    void giveGraphFocus(MouseEvent event) {
-        viewPanel.requestFocus();
-    }
+	 /**
+	  * Pan the camera on the graph view left
+	  * @param event
+	  * @Author Sean Oldfield
+	  */
+	 @FXML
+	 private void panLeftAction(ActionEvent event) {
+		 Platform.runLater(() -> {
+			 double delta = calculateDelta();
 
-    /**
-     * Set useful parameters for viewing and manipulating in the GUI
-     * @param parameters String list of parameters
-     * @Author Sean Oldfield
-     */
-    public static void setParameters(List<String> parameters) {
-        _inputFile = parameters.get(0);
-        _processors = parameters.get(1);
-        if(parameters.get(2).equals("0")) {
-            _cores = "Not Set";
-        } else {
-            _cores = parameters.get(2);
-        }
-        _outputFile = parameters.get(3);
-        _startTime = Long.parseLong(parameters.get(4));
+			 Point3 p = _camera.getViewCenter();
+			 _camera.setViewCenter(p.x - delta, p.y, 0);
+		 });
+	 }
 
-    }
+	 /**
+	  * Set the graph to be have a hierarchical layout
+	  * @param event
+	  * @Author Sean Oldfield
+	  */
+	 @FXML
+	 void setHierarchicalLayout(ActionEvent event) {
+		 _viewer.disableAutoLayout();
+		 _viewer.enableAutoLayout(new HierarchicalLayout());
+	 }
 
-    /**
-     * Calculate delta offset for zoom functions in the GUI
-     * @return The delta offset
-     * @Author Sean Oldfield
-     */
-    private double calculateDelta() {
-        return  _camera.getViewPercent() * _camera.getGraphDimension() * 0.1f;
-    }
+	 /**
+	  * Set the graph to be have a linear logarithm layout
+	  * @param event
+	  * @Author Sean Oldfield
+	  */
+	 @FXML
+	 void setLinLogLayout(ActionEvent event) {
+		 _viewer.disableAutoLayout();
+		 _viewer.enableAutoLayout(new LinLog());
+	 }
 
-    /**
-     * Start or stop the program timer
-     * @param enable True if the timer is to be started, false to stop it
-     * @Author Sean Oldfield
-     */
-    public static void toggleTimer(boolean enable) {
-        if(enable) {
-            _timer.start();
-        } else {
-            _timer.stop();
-        }
-    }
+	 /**
+	  * Set the graph to be have a spring box layout
+	  * @param event
+	  * @Author Sean Oldfield
+	  */
+	 @FXML
+	 void setSpringBoxLayout(ActionEvent event) {
+		 _viewer.disableAutoLayout();
+		 _viewer.enableAutoLayout(new SpringBox());
+	 }
 
-    // ====================================
+	 /**
+	  * Give graph view focus when clicked on i.e. allow it to be accessed by keyboard shortcuts
+	  * @param event
+	  * @Author Sean Oldfield
+	  */
+	 @FXML
+	 void giveGraphFocus(MouseEvent event) {
+		 viewPanel.requestFocus();
+	 }
 
-    private void initialisePane(int num) {
+	 /**
+	  * Set useful parameters for viewing and manipulating in the GUI
+	  * @param parameters String list of parameters
+	  * @Author Sean Oldfield
+	  */
+	 public static void setParameters(List<String> parameters) {
+		 _inputFile = parameters.get(0);
+		 _processors = parameters.get(1);
+		 if(parameters.get(2).equals("0")) {
+			 _cores = "Not Set";
+		 } else {
+			 _cores = parameters.get(2);
+		 }
+		 _outputFile = parameters.get(3);
+		 _startTime = Long.parseLong(parameters.get(4));
 
-		for (int i = 0; i < num*Integer.parseInt(_processors); i++) {
-			Pane p = new Pane();
-			p.setPrefSize(_cellWidth, _cellHeight);
-			p.setStyle("-fx-background-color: #D3D3D3");
-			tile.getChildren().add(p);
-		}
-	}
+	 }
 
-    private void initialiseLabel(int num) {
+	 /**
+	  * Calculate delta offset for zoom functions in the GUI
+	  * @return The delta offset
+	  * @Author Sean Oldfield
+	  */
+	 private double calculateDelta() {
+		 return  _camera.getViewPercent() * _camera.getGraphDimension() * 0.1f;
+	 }
 
-    	for (int i = 1; i < num+1; i++) {
+	 /**
+	  * Start or stop the program timer
+	  * @param enable True if the timer is to be started, false to stop it
+	  * @Author Sean Oldfield
+	  */
+	 public static void toggleTimer(boolean enable) {
+		 if(enable) {
+			 _timer.start();
+		 } else {
+			 _timer.stop();
+		 }
+	 }
 
-    		Label colLabel = new Label(Integer.toString(i));
-    		colLabel.setPrefSize(30, _cellHeight);
-    		colLabel.setAlignment(Pos.CENTER_RIGHT);
+	 // ====================================
 
-    		colLabelTile.getChildren().add(colLabel);
+	 private void initialisePane(int num) {
 
-		}
+		 for (int i = 0; i < Integer.parseInt(_processors); i++) {
 
-    }
+			 Label processorLabel = new Label("P" + Integer.toString(i+1));
+			 processorLabel.setPrefSize(_cellWidth, _cellHeight);
+			 processorLabel.setAlignment(Pos.CENTER);
 
-/*    public void updateSchedule(List<Task> schedule) {
+			 tile.getChildren().add(processorLabel);
+		 }
+
+		 for (int i = 0; i < (num)*Integer.parseInt(_processors); i++) {
+			 Pane p = new Pane();
+			 p.setPrefSize(_cellWidth, _cellHeight);
+			 p.setStyle("-fx-background-color: #D3D3D3");
+			 tile.getChildren().add(p);
+		 }
+
+
+	 }
+
+	 private void initialiseLabel(int num) {
+
+		 Label colLabel;
+
+		 for (int i = 0; i < num+1; i++) {
+
+			 if (i == 0 ) {
+				 colLabel = new Label("");
+			 } else {
+				 colLabel = new Label(Integer.toString(i-1));
+			 }
+			 
+			 colLabel.setPrefSize(30, _cellHeight);
+			 colLabel.setAlignment(Pos.CENTER_RIGHT);
+
+			 colLabelTile.getChildren().add(colLabel);
+		 }
+
+	 }
+
+	 /*    public void updateSchedule(List<Task> schedule) {
         for(Task task: schedule) {
             setCell(task.getNode(), task.getProcessor(), task.getStartTime(), task.);
         }
     }*/
 
 
-    private void setCell(String label, int processor, int startTime, int length, String colour) {
+	 private void setCell(String label, int processor, int startTime, int length, String colour) {
 
-		int cell = (processor + Integer.parseInt(_processors) * startTime) -1;
+		 int cell = (processor + Integer.parseInt(_processors) * startTime) - 1 + Integer.parseInt(_processors);
 
-		for (int i = 0; i < length; i++) {
+		 for (int i = 0; i < length; i++) {
 
-			if (i == 0) {
+			 if (i == 0) {
 
-				Label nodeLabel = new Label(label);
-				nodeLabel.setPrefSize(_cellWidth, _cellHeight);
-				nodeLabel.setAlignment(Pos.CENTER);
-				tile.getChildren().set(cell, nodeLabel);
-				tile.getChildren().get(cell).setStyle(colour);
-			}
-			tile.getChildren().get(cell).setStyle("-fx-background-color: " + colour);
-			cell = cell + Integer.parseInt(_processors);
-		}
-	}
+				 Label nodeLabel = new Label(label);
+				 nodeLabel.setPrefSize(_cellWidth, _cellHeight);
+				 nodeLabel.setAlignment(Pos.CENTER);
+				 tile.getChildren().set(cell, nodeLabel);
+				 tile.getChildren().get(cell).setStyle(colour);
+			 }
+			 tile.getChildren().get(cell).setStyle("-fx-background-color: " + colour);
+			 cell = cell + Integer.parseInt(_processors);
+		 }
+	 }
 
-	private String generateColours() {
+	 private String generateColours() {
 
-		Random rand = new Random();
+		 Random rand = new Random();
 
-		float r = rand.nextFloat();
-		float g = rand.nextFloat() / 2f;
-		float b = rand.nextFloat() / 2f;
+		 float r = rand.nextFloat();
+		 float g = rand.nextFloat() / 2f;
+		 float b = rand.nextFloat() / 2f;
 
-		java.awt.Color randomColor = new java.awt.Color(r, g, b);
-		String hex = Integer.toHexString(randomColor.getRGB() & 0xffffff);
+		 java.awt.Color randomColor = new java.awt.Color(r, g, b);
+		 String hex = Integer.toHexString(randomColor.getRGB() & 0xffffff);
 
-		if (hex.length() < 6) {
-			hex = "0" + hex;
-		}
-		hex = "#" + hex;
+		 if (hex.length() < 6) {
+			 hex = "0" + hex;
+		 }
+		 hex = "#" + hex;
 
-		return hex;
-	}
+		 return hex;
+	 }
 
-	private void setCellSize(int processors) {
+	 private void setCellSize(int processors) {
 
-		int gapSize = (processors - 1)*2;
-		_cellWidth = (int)(Math.floor((300 - gapSize)/processors));
-	}
+		 int gapSize = (processors - 1)*2;
+		 _cellWidth = (int)(Math.floor((300 - gapSize)/processors));
+	 }
+
+
 }
 
